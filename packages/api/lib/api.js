cc93f39717f6bbd1698e1f4997adb43973c2eaf7
Pavel Keyzik <pavel.keyzik@gmail.com>
2020-05-04 22:59:30 +0300

    feat: initialize lerna with api and core packages

---
+++ packages/api/lib/api.js
@@ -0,0 +1,7 @@
+function getModuleInfo() {
+  return {
+    moduleName: 'API',
+  };
+}
+
+export { getModuleInfo };
---
+++ packages/core/lib/core.js
@@ -0,0 +1,7 @@
+function getModuleInfo() {
+  return {
+    moduleName: 'Core',
+  };
+}
+
+export { getModuleInfo };
